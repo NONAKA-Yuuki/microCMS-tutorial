690c931e315b767809c45770a5cbb96e00cdcf21
YuukiNonaka <guitiankouki@gmail.com>
2022-07-19 03:06:42 +0900

    お問い合わせページを追加

---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+import Layout from "./components/Layout";
+
+export default function contact() {
+    return (
+        <Layout>
+            <div>
+                <h1>お問い合わせ</h1>
+            </div>
+        </Layout>
+    )
+}
