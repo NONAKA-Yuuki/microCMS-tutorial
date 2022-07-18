93e4d34ed66ec056a514a1d053ac83836ace6f3f
YuukiNonaka <guitiankouki@gmail.com>
2022-07-19 03:09:23 +0900

    レイアウトコンポーネントとスタイルを追加

---
+++ pages/components/Layout.js
@@ -0,0 +1,21 @@
+//import Image from "next/image";
+//import Script from "next/script";
+//import Link from "next/link";
+import Header from "./Header";
+import Slider from "./Slider";
+
+import styles from "../../styles/components/layout.module.scss"
+
+export default function Layout({ children, home }) {
+    return (
+        <>
+            <Header />
+            <main className={styles.main}>
+                {home && (
+                    <Slider />
+                )}
+                {children}
+            </main>
+        </>
+    );
+}
