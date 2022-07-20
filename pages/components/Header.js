7b1486c50318f34f6b8fae4da31fb52c899f227e
YuukiNonaka <guitiankouki@gmail.com>
2022-07-21 04:26:57 +0900

    ハンバーガーメニューの開閉状態を切り替える関数と状態変数を実装

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,26 +1,36 @@
 import Link from "next/link";
-import styles from '../../styles/components/header.module.scss'
+import styles from '../../styles/components/header.module.scss';
+import React, {useState} from "react";
 
-const Header = () => (
-    <header className={styles.header} id="header">
-        <div className={styles.header__inner}>
-            <h1 className={styles.title}><Link href="/"><a>17Creative<span>サイトのキャッチコピーだお</span></a></Link></h1>
-            <nav className={styles.nav} id="js-header-menu">
-                <ul className={styles.nav_wrapper}>
-                    <li className={styles.nav_item}><Link href="/"><a>WordPress</a></Link></li>
-                    <li className={styles.nav_item}><Link href="/"><a>Arduino</a></Link></li>
-                    <li className={styles.nav_item}><Link href="/"><a>Fusion360</a></Link></li>
-                    <li className={styles.nav_item}><Link href="/"><a>Swift</a></Link></li>
-                    <li className={styles.nav_item}><Link href="contact"><a>Contact</a></Link></li>
-                </ul>
-            </nav>
-            <div className={styles.hum} id="js-header-button">
-                <span></span>
-                <span></span>
-                <span></span>
+const Header = () => {
+    const [openMenu, setOpenMenu] = useState(false);
+    
+    const menuFunction = () => {
+        setOpenMenu(!openMenu);
+    }
+
+    return (
+        <header className={styles.header} id="header">
+            <div className={styles.header__inner}>
+                <h1 className={styles.title}><Link href="/"><a>17Creative<span>サイトのキャッチコピーだお</span></a></Link></h1>
+                <nav className={styles.nav} id="js-header-menu">
+                    <ul className={styles.nav_wrapper}>
+                        <li className={styles.nav_item}><Link href="/"><a>WordPress</a></Link></li>
+                        <li className={styles.nav_item}><Link href="/"><a>Arduino</a></Link></li>
+                        <li className={styles.nav_item}><Link href="/"><a>Fusion360</a></Link></li>
+                        <li className={styles.nav_item}><Link href="/"><a>Swift</a></Link></li>
+                        <li className={styles.nav_item}><Link href="contact"><a>Contact</a></Link></li>
+                    </ul>
+                </nav>
+                <div className={styles.hum} onClick={() => menuFunction()}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
             </div>
-        </div>
-    </header>
-);
+        </header>
+    )
+    
+};
 
 export default Header;
